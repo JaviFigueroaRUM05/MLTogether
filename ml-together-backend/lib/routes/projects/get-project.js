76da3ecfb8f023535cb29d166f9a0d6e5a31f994
ortizcapetta <alemariortiz@yahoo.com>
2020-04-01 21:16:53 -0400

    change sample route to reflect db items

--- ml-together-backend/lib/routes/projects/get-project.js
+++ ml-together-backend/lib/routes/projects/get-project.js
@@ -5,12 +5,33 @@ module.exports = {
   path: '/projects/{projectId}',
   options: {
     handler: async (request, h) => {
+      //Sample of what projects MongoDB might return
+      //TODO: add validators actual functionality with the DB 
       return {
         project: {
           id: request.params.projectId,
-          description: "Hey there"
+          name: "sample project",
+          description: "this is a project",
+          //save a file ? 
+          model: 'model script',
+          functions:{
+            //not save as string
+            map: 'const map = (n) => { var result = 1; return result;}',
+
+            reduce: "reduce"
+            },
+          data: {
+            url: "www.dataserver.com",
+            apikey: "ABCDEFGHIJK"
+          },
+          brokerconfig: {
+              chunksize: 2,
+              datalength: 5,
+          },
+                   
         }
       }
     }
   }
+
 };
