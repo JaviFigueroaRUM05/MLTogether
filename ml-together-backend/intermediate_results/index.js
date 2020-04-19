e897d1c87832a87f54a4c6cf54927a12888f5384
captain-yuca <manuel.baez@upr.edu>
2020-04-19 14:49:33 -0400

    Changed styling

--- ml-together-backend/intermediate_results/index.js
+++ ml-together-backend/intermediate_results/index.js
@@ -1,6 +1,8 @@
 'use strict';
-const Joi = require('@hapi/joi')
-const handler = require('../intermediate_results/results-handlers')
+
+const Joi = require('@hapi/joi');
+const Handler = require('../intermediate_results/results-handlers');
+
 exports.plugin = {
 
     name: 'IntermediateResults',
@@ -9,39 +11,39 @@ exports.plugin = {
         //TODO: better-format this plugin (separate elements)
         //TODO: review route definitions
         //TODO: Error handling
-        server.route([
-        {
+        await server.route([
+            {
             //testing
-            method: 'GET',
-            path: '/ir',
-            handler: handler.getIRs
-        },
-        {
-            method: 'GET',
-            path: '/projects/{projectId}/ir/{mapResultsId}',
-            
-            handler: handler.GetIRByProjectID,
-            options: {
-                validate: {    
+                method: 'GET',
+                path: '/ir',
+                handler: Handler.getIRs
+            },
+            {
+                method: 'GET',
+                path: '/projects/{projectId}/ir/{mapResultsId}',
+
+                handler: Handler.GetIRByProjectID,
+                options: {
+                    validate: {
                         params: Joi.object({
                             mapResultsId: Joi.number(),
                             projectId: Joi.string()
                         })
+                    }
                 }
-            }
-        },
-        {
-            method: 'POST',
-            path: '/projects/{projectId}/ir',
-            handler: handler.createIR,
-            options: {
-                validate: { 
-                    failAction: async (request, h, err) => {                      
-                       //TODO: change this to appear in debug only
-                          console.error(err);
-                          throw err;        
-                      }, 
-                        params: 
+            },
+            {
+                method: 'POST',
+                path: '/projects/{projectId}/ir',
+                handler: Handler.createIR,
+                options: {
+                    validate: {
+                        failAction: (request, h, err) => {
+                            //TODO: change this to appear in debug only
+                            console.error(err);
+                            throw err;
+                        },
+                        params:
                             Joi.object({
                                 projectId: Joi.string()
                             })
@@ -50,33 +52,33 @@ exports.plugin = {
                             mapResultsId: Joi.number(),
                             results: Joi.any()
                         })
+                    }
                 }
-            }
-        },
+            },
 
-        {
-            method: 'DELETE',
-            path: '/projects/{projectId}/ir',
-            //delete all intermediate results linked to a project
-            handler: handler.removeProjectIRs,
-            options: {
-                validate: { 
-                    failAction: async (request, h, err) => {
-                      
-                       //TODO: change this to appear in debug only
-                          console.error(err);
-                          throw err;
-        
-                      }, 
-                        params: 
+            {
+                method: 'DELETE',
+                path: '/projects/{projectId}/ir',
+                //delete all intermediate results linked to a project
+                handler: Handler.removeProjectIRs,
+                options: {
+                    validate: {
+                        failAction: (request, h, err) => {
+
+                            //TODO: change this to appear in debug only
+                            console.error(err);
+                            throw err;
+
+                        },
+                        params:
                             Joi.object({
                                 projectId: Joi.string()
                             })
-                        
+
+                    }
                 }
             }
-        }
-    ]);
-      
+        ]);
+
     }
 };
--- ml-together-backend/intermediate_results/results-handlers.js
+++ ml-together-backend/intermediate_results/results-handlers.js
@@ -1,57 +1,58 @@
+'use strict';
+
 const Boom = require('boom');
 
 //TODO: Error validation Boom
-   async function getIRs(request, h) {
-        const db = request.mongo.db;
-        let projects;
-        projects = await db.collection('intermediateResults').find().toArray();                  
-            
-        return h.response(projects).code(200);
-        }
-  
-  async function GetIRByProjectID(request,h){
-        const db = request.mongo.db;               
-        let results;
+const getIRs = async function (request, h) {
 
-        results = await db.collection('intermediateResults').find({projectId: request.params.projectId,mapResultsId: request.params.mapResultsId }).toArray();
-        
+    const db = request.mongo.db;
+    const projects = await db.collection('intermediateResults').find().toArray();
+
+    return h.response(projects).code(200);
+};
+
+const GetIRByProjectID = async function (request, h) {
+
+    const db = request.mongo.db;
+    const results = await db.collection('intermediateResults').find({ projectId: request.params.projectId, mapResultsId: request.params.mapResultsId }).toArray();
+
     return h.response(results).code(200);
-    }
+};
 
-    async function createIR(request,h){
-        const db = request.mongo.db;
-        const payload = request.payload;   
-        //adding the pid         
-        payload.projectId = request.params.projectId;
-         let results;
-         //TODO: additional payload json validation
-             
-         console.log(request.payload)
-         results = await db.collection('intermediateResults').insertOne(payload);
-            
- 
-         return h.response(results).code(201);
-    }
+const createIR = async function (request, h) {
 
-    async function removeProjectIRs(request,h){
-        const db = request.mongo.db;           
-        let results;
-        //TODO: additional payload json validation
-            
-       
-        results = await db.collection('intermediateResults').deleteMany({projectId: request.params.projectId});
-           
+    const db = request.mongo.db;
+    const payload = request.payload;
+    //adding the pid
+    payload.projectId = request.params.projectId;
+    //TODO: additional payload json validation
 
-        return h.response(results).code(200);
-    }
+    console.log(request.payload);
+    const results = await db.collection('intermediateResults').insertOne(payload);
 
 
-    
+    return h.response(results).code(201);
+};
 
-  
-module.exports= {
+const removeProjectIRs = async function (request, h) {
+
+    const db = request.mongo.db;
+    //TODO: additional payload json validation
+
+
+    const results = await db.collection('intermediateResults').deleteMany({ projectId: request.params.projectId });
+
+
+    return h.response(results).code(200);
+};
+
+
+
+
+
+module.exports = {
     getIRs,
     GetIRByProjectID,
     createIR,
     removeProjectIRs
-}
+};
