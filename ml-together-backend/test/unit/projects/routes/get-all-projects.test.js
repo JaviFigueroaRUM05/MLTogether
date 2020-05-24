a78729236641daced98b86ec6a78e583be8ac4ea
captain-yuca <manuel.baez@upr.edu>
2020-05-24 16:19:33 -0400

    Added tests for getting all projects

--- ml-together-backend/test/unit/projects/routes/get-all-projects.test.js
+++ ml-together-backend/test/unit/projects/routes/get-all-projects.test.js
@@ -14,17 +14,17 @@ const { deleteTestUsers, deleteTestProjects, getTestProjects } = require('../../
 const { experiment, it, beforeEach } = exports.lab = Lab.script();
 const { expect } = Code;
 
-experiment('Get Project', () => {
+experiment('Get Projects', () => {
 
     let server;
     let email;
     let password;
     let fullName;
-    let token;
-    let title;
-    let description;
-    let project;
-    let getProjectRoute;
+    let title1;
+    let description1;
+    let title2;
+    let description2;
+    let getProjectsRoute;
 
     beforeEach( async () => {
 
@@ -32,8 +32,10 @@ experiment('Get Project', () => {
         email = Faker.internet.email();
         password = Faker.internet.password(16, false);
 
-        title = Faker.lorem.words(5);
-        description = Faker.lorem.words(50);
+        title1 = Faker.lorem.words(5);
+        description1 = Faker.lorem.words(50);
+        title2 = Faker.lorem.words(5);
+        description2 = Faker.lorem.words(50);
 
 
         server = Hapi.server();
@@ -59,9 +61,10 @@ experiment('Get Project', () => {
         await deleteTestProjects();
 
         const { userService, projectService } = server.services();
-        token = await userService.registerUser(fullName,email, password);
-        project = await projectService.createProject(title, description);
-        getProjectRoute = `/projects/${project._id}`;
+        await userService.registerUser(fullName,email, password);
+        await projectService.createProject(title1, description1);
+        await projectService.createProject(title2, description2);
+        getProjectsRoute = `/projects`;
 
     });
 
@@ -70,13 +73,13 @@ experiment('Get Project', () => {
         expect(server.registrations[LibPlugin.pkg.name]).to.exist();
     });
 
-    it('gets a project successfully with existing project id', async () => {
+    it('gets both projects successfully', async () => {
 
         let response;
         try {
             response = await server.inject({
                 method: 'GET',
-                url: getProjectRoute
+                url: getProjectsRoute
             });
         }
         catch (err) {
@@ -86,35 +89,13 @@ experiment('Get Project', () => {
 
         expect(response.statusCode).to.be.equal(200);
 
-        const project = JSON.parse(response.payload);
-
-        expect(project).to.include(['_id', 'title', 'description']);
-        expect(project.title).to.equal(title);
-        expect(project.description).to.equal(description);
-
-    });
-
-    it('returns not found when project does not exist', async () => {
-
-        const nonexistantProjectId = Faker.random.alphaNumeric(12);
-
-        const getNonexistantProjectRoute = `/projects/${nonexistantProjectId}`;
-
-        console.log(nonexistantProjectId);
-        let response;
-        try {
-            response = await server.inject({
-                method: 'GET',
-                url: getNonexistantProjectRoute
-            });
-        }
-        catch (err) {
-            console.error(err);
-            return false;
-        }
-
-        expect(response.statusCode).to.be.equal(404);
-
+        const projects = await getTestProjects();
+        expect(projects).to.have.length(2);
+        expect(projects[0]).to.include(['_id', 'title', 'description']);
+        expect(projects[0].title).to.equal(title1);
+        expect(projects[0].description).to.equal(description1);
+        expect(projects[1].title).to.equal(title2);
+        expect(projects[1].description).to.equal(description2);
 
     });
 });
