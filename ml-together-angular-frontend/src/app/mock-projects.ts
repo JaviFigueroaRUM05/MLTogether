542decfa95ae1f1b09d9dcfdadc69576626e4348
kelvin123459 <kelvin.garcia5@upr.edu>
2020-05-28 20:51:09 -0400

    Fixed project-list issue

--- ml-together-angular-frontend/src/app/mock-projects.ts
+++ ml-together-angular-frontend/src/app/mock-projects.ts
@@ -1,14 +1,14 @@
-// import {Project} from './project'
+import {Project} from './project'
 
-// export const Projects: Project[] = [
-//     { id: 11, name: 'Dr Nice', author: 'me', description: 'lorem ipsum' },
-//     { id: 12, name: 'Narco', author: 'me', description: 'lorem ipsum' },
-//     { id: 13, name: 'Bombasto', author: 'me', description: 'lorem ipsum' },
-//     { id: 14, name: 'Celeritas', author: 'me', description: 'lorem ipsum' },
-//     { id: 15, name: 'Magneta', author: 'me', description: 'lorem ipsum' },
-//     { id: 16, name: 'RubberMan', author: 'me', description: 'lorem ipsum' },
-//     { id: 17, name: 'Dynama', author: 'me', description: 'lorem ipsum' },
-//     { id: 18, name: 'Dr IQ', author: 'me', description: 'lorem ipsum' },
-//     { id: 19, name: 'Magma', author: 'me', description: 'lorem ipsum' },
-//     { id: 20, name: 'Tornado', author: 'me', description: 'lorem ipsum' }
-//   ];
+export const Projects: Project[] = [
+    { _id: 11, name: 'Dr Nice', author: 'me', description: 'lorem ipsum' },
+    { _id: 12, name: 'Narco', author: 'me', description: 'lorem ipsum' },
+    { _id: 13, name: 'Bombasto', author: 'me', description: 'lorem ipsum' },
+    { _id: 14, name: 'Celeritas', author: 'me', description: 'lorem ipsum' },
+    { _id: 15, name: 'Magneta', author: 'me', description: 'lorem ipsum' },
+    { _id: 16, name: 'RubberMan', author: 'me', description: 'lorem ipsum' },
+    { _id: 17, name: 'Dynama', author: 'me', description: 'lorem ipsum' },
+    { _id: 18, name: 'Dr IQ', author: 'me', description: 'lorem ipsum' },
+    { _id: 19, name: 'Magma', author: 'me', description: 'lorem ipsum' },
+    { _id: 20, name: 'Tornado', author: 'me', description: 'lorem ipsum' }
+  ];
